884d504443fa2cc61d3f909a03ce94dbc12a211a
chenghao.pan <panchenghao@gaozhe.com.cn>
2024-10-16 20:44:36 +0800

    添加商品

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,8 +1,19 @@
 import request from "@/utils/request";
-import type { TradeMarkResponseData } from "./type";
+import type { TradeMarkResponseData,TradeMark} from "./type";
 
 enum API {
-    LOGIN_URL = '/admin/product/baseTrademark/'
+    LOGIN_URL = '/admin/product/baseTrademark/',
+    ADDTRADEMARK_URL = '/admin/product/baseTrademark/save',
 }
 
 export const reqHasTrademark = (page:number,limit:number) => { return request.get<any,TradeMarkResponseData>(API.LOGIN_URL+`${page}/${limit}`) }
+
+export const reqAddorUpdateTradeMark = (data:TradeMark) => { 
+    // 修改数据
+    if(data.id) {
+        return request.put<any,any>(API.ADDTRADEMARK_URL,data);
+    }else {
+    //添加数据
+        return request.post<any,any>(API.ADDTRADEMARK_URL,data);
+    }
+ }
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,4 +1,5 @@
 <template>
+    <el-button type="primary" style="margin-bottom: 20px;" @click="addProduct">添加商品</el-button>
     <el-card style="max-width: 100%">
         <el-table :data="tabledata" style="width: 100%" border>
             <el-table-column label="序号" width="180" align="center" type="index">
@@ -10,7 +11,7 @@
             </el-table-column>
             <el-table-column label="品牌LOGO" align="center">
                 <template #default="scope">
-                    <pre> {{scope.row.tmName}} </pre>
+                    <pre> {{ scope.row.tmName }} </pre>
                 </template>
             </el-table-column>
             <el-table-column label="品牌操作" align="center">
@@ -22,16 +23,38 @@
         </el-table>
 
         <el-pagination v-model:current-page="pageNo" v-model:page-size="limit" :page-sizes="[3, 5, 7, 9]"
-            layout="sizes, prev, pager, next,->,jumper,total," :total="total" @size-change="currentSizePageChange" @current-change="currentPageChange"
-            style="margin-top: 20px;" />
+            layout="sizes, prev, pager, next,->,jumper,total," :total="total" @size-change="currentSizePageChange"
+            @current-change="currentPageChange" style="margin-top: 20px;" />
     </el-card>
+
+    <el-dialog v-model="dialogVisible" title="添加品牌" style="width: 600px;">
+        <el-form>
+            <el-form-item label="品牌名称">
+                <el-input v-model="trademarkParams.tmName"></el-input>
+            </el-form-item>
+            <el-form-item label="品牌LOGO">
+                <el-upload class="avatar-uploader" action="/api/admin/product/fileUpload"
+                    :show-file-list="false" :on-success="handleAvatarSuccess">
+                    <img v-if="trademarkParams.logoUrl" :src="trademarkParams.logoUrl" class="avatar" />
+                    <el-icon v-else class="avatar-uploader-icon">
+                        <Plus />
+                    </el-icon>
+                </el-upload>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+                <el-button @click="cancel">取消</el-button>
+                <el-button type="primary" @click="sure_">确定</el-button>
+        </template>
+    </el-dialog>
 </template>
 
 <script setup lang="ts">
 //引入组合式API函数
 import { ref, onMounted, reactive } from 'vue'
-import { reqHasTrademark } from '@/api/product/trademark'
-import type { Records } from '@/api/product/trademark/type'
+import { reqHasTrademark , reqAddorUpdateTradeMark } from '@/api/product/trademark'
+import type { Records,TradeMark } from '@/api/product/trademark/type'
+import { ElMessage } from 'element-plus';
 // import {Edit,Delete} from '@element-plus/icons-vue'
 
 const pageNo = ref<number>(1)
@@ -42,6 +65,13 @@ let total = ref<number>(0)
 
 let tabledata = ref<Records>()
 
+let dialogVisible = ref<boolean>(false);
+
+let trademarkParams = reactive<TradeMark>({
+    tmName: '',
+    logoUrl: ''
+})
+
 onMounted(async () => {
     let result = await reqHasTrademark(pageNo.value, limit.value);
     if (result.code == 200) {
@@ -50,22 +80,70 @@ onMounted(async () => {
     }
 })
 
-const currentPageChange = async(value:number)=>{
-     let result = await reqHasTrademark(value, limit.value);
-     if (result.code == 200) {
+const currentPageChange = async (value: number) => {
+    let result = await reqHasTrademark(value, limit.value);
+    if (result.code == 200) {
         total.value = result.data.total;
         tabledata.value = result.data.records;
     }
 }
 
-const currentSizePageChange = async(value:number)=>{
+const currentSizePageChange = async (value: number) => {
     let result = await reqHasTrademark(pageNo.value, value);
     if (result.code == 200) {
         total.value = result.data.total;
         tabledata.value = result.data.records;
     }
-} 
+}
+
+const cancel = () => {
+    dialogVisible.value = false;
+}
+
+const sure_ = async() => {
+    dialogVisible.value = false;
+    await reqAddorUpdateTradeMark(trademarkParams);
+    ElMessage ({
+        type:"success",
+        message:'添加品牌成功'
+    })
+}
+
+const addProduct = () => {
+    trademarkParams.logoUrl='';
+    trademarkParams.tmName='';
+    trademarkParams.id=0;
+    dialogVisible.value = true;
+}
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (
+    response,
+    uploadFile
+) => {
+    trademarkParams.logoUrl = URL.createObjectURL(uploadFile.raw!)
+}
 
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.avatar-uploader .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+  }
+  
+  .avatar-uploader .el-upload:hover {
+    border-color: var(--el-color-primary);
+  }
+  
+  .el-icon.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    text-align: center;
+  }
+</style>
